63ebc714d4e75b3d3d92f29aa9c10c711bddcdea
Szymon Matyla <szymon.matyla@atos.net>
2024-08-27 10:04:53 +0200

    feature c - next modifications

--- featureA-test-add-on.py
+++ featureA-test-add-on.py
@@ -5,5 +5,5 @@ class SimpleTests(TestCase):
     def __init__(self) -> None:
         return 'Tests Add on'
     
-    def someTestMethod(self, object: str): 
+    def someTestMethod(self, object: str, other_numeric: int): 
         return equal True
